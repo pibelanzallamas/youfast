d6f9d60f357a5ff6df38d8bfd4dca62ecacb13de
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-05-17 19:58:20 -0300

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { HashRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Analytics>
+        <App />
+      </Analytics>
     </HashRouter>
   </React.StrictMode>
 );
